9b94c96fe3ff56bdbcbd4bad9b56917953c31317
Arminas <arminasb7@gmail.com>
2016-08-15 21:45:37 +0100

    Initial commit
    Add label functionality added with prompt

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,25 @@
-console.log("JS ON")
+$(function(){
+  
+
+  //Labels UI
+    //add new label
+  $('.btn-add').on('click', function(){
+    var askLabel = prompt("Please enter label name");
+    //if there is nothing enter, add non breakable space to keep size and width of the button.
+    if(askLabel === "" || askLabel === null) {
+      askLabel = "&nbsp";
+    }
+    var newLabel = ('<button class="btn btn-blue btn-blue-new">' + askLabel + '</button>'); 
+    $(newLabel).insertBefore(this);
+  })
+
+
+
+
+
+
+
+
+
+
+}); //END OF JQUERY
