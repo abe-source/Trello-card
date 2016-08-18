d3b6dec2910e6645862f46d24de09db0422644c4
Arminas <arminasb7@gmail.com>
2016-08-18 21:19:58 +0100

    initial commit
    add checklist functionality in progress

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -29,13 +29,13 @@ $(function(){
   $('.btn-save-description').on('click', function() {
       saveUserEntry();
   })
-
+      //if user pres enter and textarea is open it saves user entered description
   $('.description-outter').keyup(function(event){
     if(event.keyCode == 13){
         saveUserEntry();
     }
 });
-
+      //function to save user entry
   function saveUserEntry() {
       checkUserDescription();
       var userDescription = $('.description-area').val();
@@ -59,11 +59,74 @@ $(function(){
     $('.description').hide(50);
     $('.btn-edit-description').show();
   }
-
+      //when user pres edit or press directly on user desription textarea becomes visible
   $('.btn-edit-saved, .description-entered').on('click', function(){
     $('.description-entered').hide();
     $('.description-outter').show();
   })
+
+  //CHECKLIST
+      
+      //add new checklist
+      var idCounter = 4;
+      //if user press add item button it hides it and shows textarea to enter button name.
+      $('.add-item').on('click', function(){
+        $('.add-checkbox').hide();
+        $('.edit-checkbox-out').show();              
+      });
+
+      //when user pres add button it saves text area value and adds checklist with that value
+      $('.btn-save-checkbox').on('click', function(){
+        var checkBoxName = $('.save-checkbox').val();
+        createCheckbox(checkBoxName);
+        checkBoxName = "";
+      });
+
+      function createCheckbox(name) {
+        //fallback if user didn't entered nothing or cancel prompt window.
+        if (name === "" || name === null) {
+          name = "&nbsp";
+        }
+        $('.checkbox').append('<br><input id="check' + idCounter + '" type="checkbox" name="check"><label for="check' + idCounter + '">' + name + '</label>');
+        idCounter++;
+      }
+
+      //change checlist name  
+
+
+
+
+      //dynamicly change checkbar depending of completed tasks
+
+
+
+
+
+
+      //hide done tasks
+
+
+
+
+
+      //show how many tasks is closed
+
+
+
+
+
+      //delete all checklist
+
+
+
+
+
+
+
+      // + bonus drag and drop item changing their place
+
+
+
 
 
 
